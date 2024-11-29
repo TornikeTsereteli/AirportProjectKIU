599110546afca8490bd2db856f7691fe5892e367
tornike tsereteli <nikolozchiradze28@gmail.com>
2024-11-29 17:40:17 +0400

    first commit

---
+++ next-backend/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
+
+
+use Illuminate\Support\Facades\Mail;
+
+Route::get('/send-test-email', function () {
+    Mail::raw('Test email from Laravel', function ($message) {
+        $message->to('tornike.tsereteli2003@gmail.com')
+            ->subject('Test Email');
+    });
+
+    return 'Test email sent!';
+});
+
+
+require __DIR__.'/auth.php';
